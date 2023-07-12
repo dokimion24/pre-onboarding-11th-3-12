428482f0021d6884c2c3babe49d0195959b86c5f
dokimion24 <leecy3610@gmail.com>
2023-07-12 15:02:29 +0900

    Design: Layout 스타일 추가

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,9 +1,18 @@
+/** @jsxImportSource @emotion/react */
+
 import { Outlet } from 'react-router-dom';
 import Header from '../Header';
 
 const Layout = () => {
   return (
-    <div>
+    <div
+      css={{
+        maxWidth: '500px',
+        height: '100%',
+        backgroundColor: '#fff',
+        padding: '16px',
+      }}
+    >
       <Header />
       <Outlet />
     </div>
