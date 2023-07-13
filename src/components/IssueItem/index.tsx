efafffb6069917ec8a0a3d9b89310c6f09755692
dokimion24 <leecy3610@gmail.com>
2023-07-13 10:33:54 +0900

    Feat: 이슈 클릭 시 디테일로 이동

--- src/components/IssueItem/index.tsx
+++ src/components/IssueItem/index.tsx
@@ -1,16 +1,27 @@
 /** @jsxImportSource @emotion/react */
 import React from 'react';
 import Txt from '../Txt';
+import { useNavigate } from 'react-router-dom';
+import { useIssuesDispatch } from '../../contexts/IssuesContext';
 
 const IssueItem = ({ issue }: any) => {
-  const { created_at, comments, title, user, number } = issue;
+  const navigate = useNavigate();
+  const dispatch = useIssuesDispatch();
+  const { created_at, comments, title, user, number, id } = issue;
+
+  const handleClick = () => {
+    dispatch({ type: 'GET_ISSUE_DETAIL', payload: id });
+    navigate(`/detail/${id}`);
+  };
 
   return (
     <li
+      onClick={handleClick}
       css={{
         display: 'flex',
         borderBottom: '1px solid #ccc',
         padding: '12px 0px',
+        cursor: 'pointer',
       }}
     >
       <div css={{ width: '400px' }}>
