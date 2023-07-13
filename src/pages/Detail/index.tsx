c9e7f65ad544ae0829e4e5781b6079cbac977426
dokimion24 <leecy3610@gmail.com>
2023-07-13 10:38:32 +0900

    Feat: Detail

--- src/pages/Detail/index.tsx
+++ src/pages/Detail/index.tsx
@@ -1,7 +1,17 @@
 import React from 'react';
+import Header from '../../components/Header';
+import IssueDetail from '../../components/IssueDetail';
+import { useIssuesState } from '../../contexts/IssuesContext';
 
 const Detail = () => {
-  return <div>Detail</div>;
+  const state = useIssuesState();
+
+  return (
+    <>
+      <Header />
+      <IssueDetail issue={state.data} />
+    </>
+  );
 };
 
 export default Detail;
