c67c8bf551b6dfcda91914607fa1ee32be4b827f
dokimion24 <leecy3610@gmail.com>
2023-07-13 10:35:03 +0900

    Feat: IssuesContext 작성

--- src/contexts/IssuesContext.tsx
+++ src/contexts/IssuesContext.tsx
@@ -10,86 +10,42 @@ type Action =
   | { type: 'GET_ISSUES' }
   | { type: 'GET_ISSUES_SUCCESS'; data: any }
   | { type: 'GET_ISSUES_ERROR'; error: any }
-  | { type: 'GET_ISSUE' }
-  | { type: 'GET_ISSUE_SUCCESS'; data: any }
-  | { type: 'GET_ISSUE_ERROR'; error: any };
+  | { type: 'GET_ISSUE_DETAIL'; payload: string };
 
-type InitialState = {
-  issues: {
-    loading: boolean;
-    data: any;
-    error: any;
-  };
-  issue: {
-    loading: boolean;
-    data: any;
-    error: any;
-  };
-};
-
-const initialState: InitialState = {
-  issues: {
-    loading: false,
-    data: null,
-    error: null,
-  },
-  issue: {
-    loading: false,
-    data: null,
-    error: null,
-  },
-};
-
-const loadingState = {
-  loading: true,
+const initialState: State = {
+  loading: false,
   data: null,
   error: null,
 };
 
-const success = (data: any): State => ({
-  loading: false,
-  data,
-  error: null,
-});
-
-const error = (error: any): State => ({
-  loading: false,
-  data: null,
-  error: error,
-});
-
-const issuesReducer = (state: State, action: Action): any => {
+const issuesReducer = (state: State, action: Action): State => {
   switch (action.type) {
     case 'GET_ISSUES':
       return {
         ...state,
-        issues: loadingState,
+        loading: true,
       };
     case 'GET_ISSUES_SUCCESS':
       return {
-        ...state,
-        issues: success(action.data),
+        loading: false,
+        data: action.data,
+        error: null,
       };
     case 'GET_ISSUES_ERROR':
       return {
-        ...state,
-        issues: error(action.error),
+        loading: false,
+        data: null,
+        error: action.error,
       };
-    case 'GET_ISSUE':
+    case 'GET_ISSUE_DETAIL':
+      const filteredData = state.data.filter(
+        (issue: any) => issue.id === action.payload,
+      );
       return {
         ...state,
-        issue: loadingState,
+        data: filteredData,
       };
-    case 'GET_ISSUE_SUCCESS':
-      return {
-        ...state,
-        issue: success(action.data),
-      };
-    case 'GET_ISSUE_ERROR':
-      return {
-        ...state,
-        issue: error(action.error),
-      };
+
     default:
       throw new Error(`Unhanded action type`);
   }
